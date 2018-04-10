6f3a6b6faa4f03d2a8998659b652604be864f792
a17emifu <kamonabe0@gmail.com>
2018-04-10 11:30:17 +0200

    Uppdated

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -2,14 +2,25 @@ package org.brohede.marcus.listviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
+
+    private String[] matterhorn = {"Matterhorn", "Alps", "4478m"};
+    private String[] montblanc = {"Mont Blanc", "Alps", "4808m"};
+    private String[] denali = {"Denali", "Alaska", "6190m"};
+
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
 
 
@@ -17,6 +28,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
+        List<String> listdata =new ArrayList<String>(Arrays.asList(mountainNames));
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,R.id.my_item_textview,listdata);
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View v, int i, long id) {
+                switch(i){
+                    case 0 :
+                        Toast.makeText(MainActivity.this, "4478m i Alps!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1 :
+                        Toast.makeText(MainActivity.this, "4808m i Alps!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2 :
+                        Toast.makeText(MainActivity.this, "6190m i Alaska!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
 
         // The onCreate method is run when the app is created.
         // Before you can implement this you need to create the layout xml files that
