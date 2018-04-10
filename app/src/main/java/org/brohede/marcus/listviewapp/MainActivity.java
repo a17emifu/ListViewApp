348a435204ec1b5aa7c84a507dcef33c22a21a16
a17emifu <kamonabe0@gmail.com>
2018-04-10 17:21:54 +0200

    Uppdated

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -17,25 +17,27 @@ public class MainActivity extends AppCompatActivity {
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
 
-    private String[] matterhorn = {"Matterhorn", "Alps", "4478m"};
-    private String[] montblanc = {"Mont Blanc", "Alps", "4808m"};
-    private String[] denali = {"Denali", "Alaska", "6190m"};
-
-    // Create ArrayLists from the raw data above and use these lists when populating your ListView.
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Arraylist för ListView
         String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
         List<String> listdata =new ArrayList<String>(Arrays.asList(mountainNames));
+
+        //Arrayadapter för ListView
         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,R.id.my_item_textview,listdata);
         ListView myListView = (ListView)findViewById(R.id.my_listview);
         myListView.setAdapter(adapter);
+        
+        //För att listitem kan tryckas på
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View v, int i, long id) {
+                //Toast för varje bergs information genom switch-satser
                 switch(i){
                     case 0 :
                         Toast.makeText(MainActivity.this, "4478m i Alps!", Toast.LENGTH_SHORT).show();
@@ -50,27 +52,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // The onCreate method is run when the app is created.
-        // Before you can implement this you need to create the layout xml files that
-        // will hold/show your data created here. You need three create things:
-        // * my_listview - the ID to the actual layout element that is our ListView.
-        //                 This my_listview element is created in the activity_main.xml file
-        // * list_item_textview - This is a new layout xml file that holds the layout for an
-        //                        individual item in the ListView we are creating
-        // * my_item_textview - This is the ID to the actual TextView that will contain the text for
-        //                      an individual item in the ListView we are creating.
-        // Here you should enter your code that fills the ListView
-        // 1. Create an array
-        // 2. Create a List object with your array from step 1 as in-data
-        // 3. Create an ArrayAdapter object that connects
-        //    * list_item_textview
-        //    * my_item_textview
-        //    * List object created in step 2
-        // 4. Find the ListView layout element "my_listview" and create an object instance
-        // 5. Connect the ArrayAdapter from step 3 with ListView object created in step 4
-        // 6. Style the ListView items according to Material Design
-        //    See: https://material.io/guidelines/components/lists.html#lists-specs
-        //    Look for "singel line specs" for "text only" lists and modify the list_item_textview
-        //    accordingly
     }
 }
